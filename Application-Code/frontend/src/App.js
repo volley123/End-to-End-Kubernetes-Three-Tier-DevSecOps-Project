ba412ab3cb36da1623c3f1ba075459f06b11b069
Varshil Desai <varshil.desai@qloudx.com>
2024-08-20 16:16:05 +0530

    updated frontend App.js

--- Application-Code/frontend/src/App.js
+++ Application-Code/frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>My To-Do List</h1>
+                    <h1>Interactive To-Do List</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
